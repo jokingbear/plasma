bb17fbbb7291146e4e280583b2e93fc48d9cafdb
simon.huynh <nerokid1072@gmail.com>
2025-06-24 11:18:31 +0000

    add IO pipe

--- plasma/functional/pipes/__init__.py
+++ plasma/functional/pipes/__init__.py
@@ -4,5 +4,5 @@ from .lambda_pipe import LambdaPipe
 from .sequential_pipe import SequentialPipe
 from .base_configs import BaseConfigs
 from .state import State
-from .simple_pipe import SimplePipe
+from .simple_pipe import SimplePipe, I2O, I3O, I4O
 from .tuple_pipe import TuplePipe
--- plasma/functional/pipes/simple_pipe.py
+++ plasma/functional/pipes/simple_pipe.py
@@ -10,3 +10,34 @@ class SimplePipe[I, O](AutoPipe):
 
     def __call__(self, inputs:I) -> O:
         return self.run(inputs)
+
+
+
+class I2O[I1, I2, O](AutoPipe):
+    
+    @abstractmethod
+    def run(self, input1:I1, input2:I2) -> O:
+        pass
+    
+    def __call__(self, input1:I1, input2:I2):
+        return self.run(input1, input2)
+    
+
+class I3O[I1, I2, I3, O](AutoPipe):
+    
+    @abstractmethod
+    def run(self, input1:I1, input2:I2, input3: I3) -> O:
+        pass
+    
+    def __call__(self, input1:I1, input2:I2, input3: I3):
+        return self.run(input1, input2, input3)
+    
+
+class I4O[I1, I2, I3, I4, O](AutoPipe):
+    
+    @abstractmethod
+    def run(self, input1:I1, input2:I2, input3: I3, input4:I4) -> O:
+        pass
+    
+    def __call__(self, input1:I1, input2:I2, input3: I3, input4:I4):
+        return self.run(input1, input2, input3, input4)
