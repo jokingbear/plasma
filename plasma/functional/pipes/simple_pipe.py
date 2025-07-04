f2d63dda076e488bb3c5aa915e54dd701a6f77d6
simon.huynh <nerokid1072@gmail.com>
2025-07-04 12:27:45 +0000

    add kwargs to simple pipes

--- plasma/functional/pipes/simple_pipe.py
+++ plasma/functional/pipes/simple_pipe.py
@@ -5,39 +5,39 @@ from abc import abstractmethod
 class SimplePipe[I, O](AutoPipe):
 
     @abstractmethod
-    def run(self, inputs:I)->O:
+    def run(self, inputs:I, **kwargs)->O:
         pass
 
-    def __call__(self, inputs:I) -> O:
-        return self.run(inputs)
+    def __call__(self, inputs:I, **kwargs) -> O:
+        return self.run(inputs, **kwargs)
 
 
 
 class I2O[I1, I2, O](AutoPipe):
     
     @abstractmethod
-    def run(self, input1:I1, input2:I2) -> O:
+    def run(self, input1:I1, input2:I2, **kwargs) -> O:
         pass
     
-    def __call__(self, input1:I1, input2:I2):
-        return self.run(input1, input2)
+    def __call__(self, input1:I1, input2:I2, **kwargs):
+        return self.run(input1, input2, **kwargs)
     
 
 class I3O[I1, I2, I3, O](AutoPipe):
     
     @abstractmethod
-    def run(self, input1:I1, input2:I2, input3: I3) -> O:
+    def run(self, input1:I1, input2:I2, input3: I3, **kwargs) -> O:
         pass
     
-    def __call__(self, input1:I1, input2:I2, input3: I3):
-        return self.run(input1, input2, input3)
+    def __call__(self, input1:I1, input2:I2, input3: I3, **kwargs):
+        return self.run(input1, input2, input3, **kwargs)
     
 
 class I4O[I1, I2, I3, I4, O](AutoPipe):
     
     @abstractmethod
-    def run(self, input1:I1, input2:I2, input3: I3, input4:I4) -> O:
+    def run(self, input1:I1, input2:I2, input3: I3, input4:I4, **kwargs) -> O:
         pass
     
-    def __call__(self, input1:I1, input2:I2, input3: I3, input4:I4):
-        return self.run(input1, input2, input3, input4)
+    def __call__(self, input1:I1, input2:I2, input3: I3, input4:I4, **kwargs):
+        return self.run(input1, input2, input3, input4, **kwargs)
