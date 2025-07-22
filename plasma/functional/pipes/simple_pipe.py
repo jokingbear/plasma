ddf84dcbbfa35e213f387bfc07fcb519a2ea1b8b
simon.huynh <nerokid1072@gmail.com>
2025-07-22 04:51:06 +0000

    sunsetting simple pipe

--- plasma/functional/pipes/simple_pipe.py
+++ plasma/functional/pipes/simple_pipe.py
@@ -1,7 +1,9 @@
 from .pipe import AutoPipe
 from abc import abstractmethod
+from warnings import deprecated
 
 
+@deprecated('use typing.Callable instead')
 class SimplePipe[I, O](AutoPipe):
 
     @abstractmethod
@@ -9,6 +11,7 @@ class SimplePipe[I, O](AutoPipe):
         pass
 
 
+@deprecated('use typing.Callable instead')
 class I2O[I1, I2, O](AutoPipe):
     
     @abstractmethod
@@ -16,6 +19,7 @@ class I2O[I1, I2, O](AutoPipe):
         pass
     
 
+@deprecated('use typing.Callable instead')
 class I3O[I1, I2, I3, O](AutoPipe):
     
     @abstractmethod
@@ -23,6 +27,7 @@ class I3O[I1, I2, I3, O](AutoPipe):
         pass
     
 
+@deprecated('use typing.Callable instead')
 class I4O[I1, I2, I3, I4, O](AutoPipe):
     
     @abstractmethod
