1a6befafe1d30b3158114e390655cd6dfbd7c61a
simon.huynh <nerokid1072@gmail.com>
2025-07-25 05:27:19 +0000

    add mergeable graph

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('requirements.txt', 'r') as handler:
 
 setup(
     name='plasma',
-    version='2.2.1a1',
+    version='2.2.2a0',
     packages=['plasma', *packages],
     url='https://github.com/jokingbear/research-idea',
     license='MIT',
