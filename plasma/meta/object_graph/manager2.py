1df83f5f5ecca629d59b22181670eab1a4770713
simon.huynh <nerokid1072@gmail.com>
2025-08-11 12:29:30 +0000

    add deprecation for decorator provider

--- plasma/meta/object_graph/manager2.py
+++ plasma/meta/object_graph/manager2.py
@@ -1,10 +1,12 @@
 from typing import Callable
 from .manager1 import Manager
 from .decorable_injector import DependencyInjector
+from warnings import deprecated
 
 
 class Manager2(Manager):
     
+    @deprecated('this method is deprecated, please use add_wrapper instead')
     def add_decorator_provider(self, decorator_provider:Callable[[str, type], type]):
         for n in self._dep_graph:
             if 'initiator' in self._dep_graph.nodes[n]:
