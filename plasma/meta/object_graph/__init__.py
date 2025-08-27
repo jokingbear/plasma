4c396f3970565f9a88c104591c3cafb9b881b593
simon.huynh <nerokid1072@gmail.com>
2025-08-27 07:19:36 +0000

    fix import error

--- plasma/meta/object_graph/__init__.py
+++ plasma/meta/object_graph/__init__.py
@@ -1,4 +1,4 @@
-from .legacy.runnable_manager import  Manager
+from .legacy import  Manager
 from .managed import (
     AutoContext as Context,
     ContextManager
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('requirements.txt', 'r') as handler:
 
 setup(
     name='plasma',
-    version='3.0.0a6',
+    version='3.0.0a7',
     packages=['plasma', *packages],
     url='https://github.com/jokingbear/research-idea',
     license='MIT',
