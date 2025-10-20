a70794b6a0710825dfa694bf1c921eb07aebeae9
jokingbear <nerokid1072@gmail.com>
2025-10-20 08:37:26 +0000

    move color to separate module

--- plasma/meta/__init__.py
+++ plasma/meta/__init__.py
@@ -1,5 +1,4 @@
 from .module_hub import ModuleHub
 from .entry_factory import entry_point, get_module_entry
-from .utils import import_module, run_cfg, load_entry, load_entries, mass_import
+from .utils import import_module, load_entry, mass_import
 from .class_registrator import ObjectFactory
-from .dependency_injector import DependencyInjector
