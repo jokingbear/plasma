14073cf9eceb86894aa57fd7bd6d62fc451bfa07
jokingbear <nerokid1072@gmail.com>
2025-10-21 03:44:06 +0000

    update init for compatibility

--- plasma/meta/__init__.py
+++ plasma/meta/__init__.py
@@ -2,3 +2,4 @@ from .module_hub import ModuleHub
 from .entry_factory import entry_point, get_module_entry
 from .utils import import_module, load_entry, mass_import
 from .class_registrator import ObjectFactory
+from .dependency_injector import DependencyInjector
