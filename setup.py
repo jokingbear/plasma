f069683740976c04c14e9e379bd939ca0fe3ea7d
jokingbear <nerokid1072@gmail.com>
2025-10-28 08:41:31 +0000

    update object registrator

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('requirements.txt', 'r') as handler:
 
 setup(
     name='plasma',
-    version='3.2.1dev7',
+    version='3.2.1dev8',
     packages=['plasma', *packages],
     url='https://github.com/jokingbear/research-idea',
     license='MIT',
