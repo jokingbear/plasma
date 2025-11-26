a479de538628287c1f26c3c5f872fbd43ab0eabb
jokingbear <nerokid1072@gmail.com>
2025-11-26 02:02:52 +0000

    update version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('requirements.txt', 'r') as handler:
 
 setup(
     name='plasma',
-    version='3.3.2a3',
+    version='3.3.3dev4',
     packages=['plasma', *packages],
     url='https://github.com/jokingbear/research-idea',
     license='MIT',
