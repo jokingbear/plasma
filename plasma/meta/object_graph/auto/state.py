dfcb97b9e66a89f384144bcc2023d054a04f8f42
jokingbear <nerokid1072@gmail.com>
2026-01-05 11:35:49 +0000

    deref state

--- plasma/meta/object_graph/auto/state.py
+++ plasma/meta/object_graph/auto/state.py
@@ -1,8 +1,4 @@
-import inspect
-
 from .context_graph import ContextGraph
-from ...utils import get_caller_frame
-from pathlib import Path
-from .functional_context import FunctionalContext
+
 
 CONTEXT_GRAPH = ContextGraph()
