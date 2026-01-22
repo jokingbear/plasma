558519973d0b1b91d906d340962379819402dbe6
jokingbear <nerokid1072@gmail.com>
2026-01-22 11:21:34 +0000

    developing meta object

--- plasma/data_model/graph/object_inquirer.py
+++ plasma/data_model/graph/object_inquirer.py
@@ -4,6 +4,9 @@ import re
 from typing import Callable
 
 
+class EmptyResult:...
+
+
 class ObjectInquirer:
     
     def __init__(self):        
@@ -12,19 +15,26 @@ class ObjectInquirer:
             pd.Series: lambda s, k: s.loc[k]
         }
     
-    def query(self, obj, chain_attr:str):
+    def query(self, obj, chain_attr:str, default=EmptyResult):
         attr_names = chain_attr.split('.')
         for n in attr_names:
             obj_type = type(self.obj)
             if re.search(r'^\d+$', n):
                 n = int(n)
-
+            
+            qresult = EmptyResult
             if obj_type in self._type_accessor:
-                obj = self._type_accessor[obj_type](obj, n)
-            elif isinstance(obj, (list, dict, tuple)):
-                obj = obj[n]
-            else:
-                obj = getattr(obj, n)
+                qresult = self._type_accessor[obj_type](obj, n)
+            elif isinstance(obj, (list, tuple)):
+                qresult = obj[n]
+            elif isinstance(obj, dict):
+                qresult = obj.get(n, qresult)
+            elif hasattr(obj, n):
+                qresult = getattr(obj, n)
+            
+            obj = qresult
+            if obj is EmptyResult:
+                break
                 
         return obj
 
