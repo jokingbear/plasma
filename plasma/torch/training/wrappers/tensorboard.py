237b9bb0fe706daa8058fb061d6861ed37b41693
jokingbear <nerokid1072@gmail.com>
2026-01-27 03:38:32 +0000

    add logger to tensorboard

--- plasma/torch/training/wrappers/tensorboard.py
+++ plasma/torch/training/wrappers/tensorboard.py
@@ -1,26 +1,40 @@
+import torch.nn as nn
+import torch.optim as optimizers
+
 from torch.utils.tensorboard import SummaryWriter
 from ..bases import ForwardWrapper
+from typing import Callable
 
 
 class Tensorboard(ForwardWrapper):
 
-    def __init__(self, log_dir, n=1):
+    def __init__(self, log_dir, n=1, 
+                logger:Callable[[object, optimizers.lr_scheduler.LRScheduler], dict[str, float]]=None
+            ):
         super().__init__()
 
         self.log_dir = log_dir
         self._writer = SummaryWriter(self.log_dir)
         self._counter = 0
         self.n = n
+        self.log_func = logger or log
 
     def append(self, trainer, i, inputs, outputs):
         if trainer.rank == 0 and outputs is not None:
-            self.log(trainer, self._counter, inputs, outputs)
+            writer = self._writer
+            infos = self.log_func(outputs, trainer.scheduler)
+            
+            for key, value in infos.items():
+                writer.add_scalar(key, value, self._counter)
         
         self._counter += 1
 
-    def log(self, trainer, step, inputs, outputs):
-        writer = self._writer
-        writer.add_scalar('loss', outputs.float(), step)
-        if trainer.scheduler is not None:
-            for i, lr in enumerate(trainer.scheduler.get_last_lr()):
-                writer.add_scalar(f'lr-{i}', lr, step)
+
+def log(outputs, scheduler:optimizers.lr_scheduler.LRScheduler) -> dict[str, float]:
+    infos = {'loss': outputs.float()}
+    
+    if scheduler is not None:
+        for i, l in enumerate(scheduler.get_last_lr()):
+            infos[f'lr-{i}'] = l
+
+    return infos
