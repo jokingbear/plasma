2e69ad2a980bab471ddfc0dde50507db4f4966ac
jokingbear <nerokid1072@gmail.com>
2026-01-29 11:08:17 +0000

    improve inquirer

--- plasma/data_model/graph/digraph/index/inquirer.py
+++ plasma/data_model/graph/digraph/index/inquirer.py
@@ -18,47 +18,49 @@ class Inquirer:
         self._successor_indices = successor_indices
         self._predcessor_indices = predecessor_indices
     
-    def nodes(self, index_value, index_name:str=None):
-        index = None
-        if index_name is None:
-            index = self._indices['type']
-        elif index_name in self._indices:
-            index = self._indices[index_name]
-        
-        results = None
-        if index is not None:
-            index_values = index_value
-            if not isinstance(index_value, list):
-                index_values = [index_value]
-            
-            results = set()
-            for v in index_values:
-                results.update(index.get(v, []))
-                
-        return results or set()
+    def nodes(self, index_values, index_name:str=None):
+        standardized_values = standardize_value(index_values)
+        index_name = index_name or 'type'
+        index = self._indices['type']        
+        results = set()
+        for v in standardized_values:
+            results.update(index.get(v, []))
+
+        return results
     
-    def successors(self, node_id, succ_type=None):
+    def successors(self, node_id, succ_types:object|list=None):
         ntype = self._type_getter(node_id)
-        if succ_type is None:
-            return self._graph.successors(node_id)
-        else:
-            succ_types = succ_type
-            if not isinstance(succ_type, list):
-                succ_types = [succ_type]
-            
-            for t in succ_types:
-                for n in self._successor_indices.get((ntype, t), []):
+        standardized_types = standardize_value(succ_types)
+        if len(standardized_types) == 0:
+            for n in self._graph.successors(node_id):
+                yield n
+        else:            
+            for t in standardized_types:
+                for n in self._successor_indices.get((ntype, t), {}).get(node_id, []):
                     yield n
             
-    def predecessors(self, node_id, pred_type=None):
+    def predecessors(self, node_id, pred_types:object|list=None):
         ntype = self._type_getter(node_id)
-        if pred_type is None:
-            return self._graph.predecessors(node_id)
+        standardized_types= standardize_value(pred_types)
+        if len(standardized_types) == 0:
+            for n in self._graph.predecessors(node_id):
+                yield n
         else:
-            pred_types = pred_type
-            if not isinstance(pred_type, list):
-                pred_types = [pred_type]
-            
-            for t in pred_types:
-                for n in self._successor_indices.get((ntype, t), []):
+            for t in standardized_types:
+                for n in self._predcessor_indices.get((ntype, t), {}).get(node_id, []):
                     yield n
+
+    def rank(self, index_values, index_name:str=None):
+        index_values = standardize_value(index_values)
+        index_name = index_name or 'type'
+        index = self._indices[index_name]
+        return sum(len(index.get(v, [])) for v in index_values)
+
+
+def standardize_value(values:object|list|None):
+    if values is None:
+        return []
+    elif isinstance(values, list):
+        return values
+    else:
+        return [values]
