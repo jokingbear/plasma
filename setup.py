93c363033a97e02d26eca676196a9e9ebe416f65
jokingbear <nerokid1072@gmail.com>
2026-01-29 01:19:44 +0000

    fix version error

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(requirement_path) as f:
 
 setup(
     name='plasma',
-    version='4.3.3a9',
+    version='4.3.2a9',
     packages=[*packages],
     license='MIT',
     author='jokingbear',
