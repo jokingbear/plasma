3d3f0c49bc288afb1b10c272a331bd9b0a54ee7c
jokingbear <nerokid1072@gmail.com>
2026-02-10 05:13:30 +0000

    fix update tuple dict

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(requirement_path) as f:
 
 setup(
     name='plasma',
-    version='4.4.11a29',
+    version='4.4.11a30',
     packages=[*packages],
     license='MIT',
     author='jokingbear',
