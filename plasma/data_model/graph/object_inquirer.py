6f0cfdf2a21406a893f73edb3fd09a7d0df46fd8
jokingbear <nerokid1072@gmail.com>
2026-02-12 03:27:24 +0000

    add direct item access

--- plasma/data_model/graph/object_inquirer.py
+++ plasma/data_model/graph/object_inquirer.py
@@ -55,9 +55,11 @@ class EmptyResult:...
 class TupleDict:
     
     def __init__(self, names:tuple, values:tuple):
-        self.names = names
+        self.__names = names
         self.values = values
-        self._name_args = {n: i for i,n  in enumerate(names)}
+        
+        name_arg_map = {n: i for i,n  in enumerate(names)}
+        self._name_arg_map = name_arg_map
     
     @property
     def items(self):
@@ -65,20 +67,26 @@ class TupleDict:
 
     def update(self, new_names, new_values):
         return TupleDict(
-            [*self.names, *new_names],
+            [*self.__names, *new_names],
             [*self.values, *new_values]
         )
     
     def keys(self):
-        for n in self.names:
+        for n in self.__names:
             yield n
     
     def __getitem__(self, name):
-        return self.values[self._name_args[name]]
+        return self.values[self._name_arg_map[name]]
     
     def __iter__(self):
         for v in self.values:
             yield v
 
     def __len__(self):
-        return len(self.names)
+        return len(self.__names)
+
+    def __getattr__(self, name):
+        if name in self._name_arg_map:
+            return self[name]
+        else:
+            return super().__getattr__(name)
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(requirement_path) as f:
 
 setup(
     name='plasma',
-    version='4.4.15a40',
+    version='4.4.16a42',
     packages=[*packages],
     license='MIT',
     author='jokingbear',
