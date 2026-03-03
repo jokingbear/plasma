07ca25ca0d4da025832b2b20be6aa42f3ae04f67
jokingbear <nerokid1072@gmail.com>
2026-03-03 01:58:41 +0000

    fix incorrect signature

--- plasma/data_model/graph/digraph/inquirer/nodes/projector.py
+++ plasma/data_model/graph/digraph/inquirer/nodes/projector.py
@@ -37,7 +37,7 @@ class Projector[T](AutoPipe[[Hashable, ObjectInquirer], TupleDict]):
                 signature = inspect.signature(f)
                 has_args = any(p.kind is inspect._ParameterKind.VAR_POSITIONAL 
                                for p in signature.parameters.values())
-                parameters = {k:p for k, p in signature.parameters if k != 'self'}
+                parameters = {k:p for k, p in signature.parameters.items() if k != 'self'}
                 try:
                     if has_args or len(parameters) > 2:
                         qresult = f(node_id, self.inquirer, results)
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(requirement_path) as f:
 
 setup(
     name='plasma',
-    version='4.5.25a105',
+    version='4.5.25a106',
     packages=[*packages],
     license='MIT',
     author='jokingbear',
