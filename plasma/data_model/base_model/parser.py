8b7da9ee21a696254e41ea8aa3db003be7aa0b60
jokingbear <nerokid1072@gmail.com>
2026-03-07 02:17:02 +0000

    add support for model flattening

--- plasma/data_model/base_model/parser.py
+++ plasma/data_model/base_model/parser.py
@@ -6,8 +6,10 @@ from ...functional import AutoPipe
 
 class Parser(AutoPipe[[object], object]):
     
-    def __init__(self):
+    def __init__(self, *, flatten_model=False):
+        super().__init__()
         self._type_solver = dict[type, Callable[[object], object]]()
+        self.flatten_model = flatten_model
     
     def run(self, data):
         data_cls = type(data)
@@ -19,7 +21,12 @@ class Parser(AutoPipe[[object], object]):
             for a in type(data).__annotations__:
                 value = getattr(data, a)
                 value = self.run(value)
-                results[a] = value
+                
+                if self.flatten_model and isinstance(value, dict):
+                    for k, v in value.items():
+                        results[f'{a}.{k}'] = v
+                else:
+                    results[a] = value
         elif isinstance(data, (tuple, list)):
             results = [self.run(v) for v in data]
         elif isinstance(data, dict):
