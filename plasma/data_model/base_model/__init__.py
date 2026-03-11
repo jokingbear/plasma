407639ea491deeee79500c75eac837b06b7e8f8b
jokingbear <nerokid1072@gmail.com>
2026-03-11 11:21:34 +0000

    fix from fields

--- plasma/data_model/base_model/__init__.py
+++ plasma/data_model/base_model/__init__.py
@@ -1,7 +1,6 @@
 from .field import Field
 from .base2 import model, register_field
-from .parser import Parser, ParsingProvider
+from .parser import Parser, ParsingProvider, FieldParser
 from .validator import Validator
 from .inquirer import is_data_model
 from .serializer import Serializer, SerializationProvider
-
--- plasma/data_model/base_model/parser/base.py
+++ plasma/data_model/base_model/parser/base.py
@@ -40,13 +40,6 @@ class Parser[T](ReadableClass):
     def from_struct(self, struct:dict) -> T:
         accessors = struct2accessor(self._struct_schema, struct)
         return self.from_accessors(accessors)
-    
-    def from_fields(self, fields:dict[Field, object]):
-        data = {}
-        for field in fields:
-            resolve(fields, field, data)
-        
-        return self.from_dict(data)
 
 
 def _construct(cls:type, struct):
@@ -78,3 +71,36 @@ def resolve(fields_values:dict[Field, object], field:Field, results:dict):
         temp_dict = temp_dict[a]
     
     temp_dict[name] = fields_values[field]
+
+
+class _Args(dict[str, object]):
+    
+    def __init__(self, root_type:type, data:dict[str, object]):
+        super().__init__()
+        
+        for field_name, field_type in root_type.__annotations__.items():
+            self.__update(field_name, field_type, data.get(field_name))
+    
+    def __update(self, field_name, field_type:type, value):
+        field_value = value
+        if value is not None:
+            if hasattr(field_type, MODEL_FLAG) and not isinstance(value, field_type):
+                assert isinstance(value, dict), 'model must have dict value'
+                field_value = ModelConstructor(field_type).from_dict(value)
+            
+            elif isinstance(value, (list, tuple)):
+                origin = get_origin(field_type)
+                if origin is not None:
+                    field_type, *_ = get_args(field_type)
+
+                if hasattr(field_type, MODEL_FLAG):
+                    field_value = []
+                    for v in value:
+                        if isinstance(v, field_type):
+                            field_value.append(v)
+                        elif isinstance(v, dict):
+                            field_value.append(ModelConstructor(field_type).from_dict(v))
+                        else:
+                            raise ValueError(f'{v} is not of type {field_type} or dict')
+
+        self[field_name] = field_value
