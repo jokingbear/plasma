e1a128532331608d7b9da730dc741ed550091c40
jokingbear <nerokid1072@gmail.com>
2026-03-11 15:13:25 +0000

    change accessor construction

--- plasma/data_model/base_model/schemas/__init__.py
+++ plasma/data_model/base_model/schemas/__init__.py
@@ -2,6 +2,8 @@ from .accessors import AccessorSchema
 from .struct import StructSchema
 from .utils import struct2accessor, accessor2struct
 from .base import Schema
+from .realization import Realization
+from .representation import GraphRepresetation
 from ..constants import MODEL_FLAG
 
 
