557a5ea4dc51aaa24c740ebd8e9f2ec1111ebec3
jokingbear <nerokid1072@gmail.com>
2026-03-12 01:31:04 +0000

    update validator

--- plasma/data_model/base_model/validator.py
+++ plasma/data_model/base_model/validator.py
@@ -1,12 +1,7 @@
-import re
-
-from typing import get_args, get_origin
 from enum import Enum
 
 from .inquirer import is_data_model
-from .schemas import AccessorSchema
-from .field import Field, List
-from .serializer import Serializer
+from .schemas import schema
 from ...functional import AutoPipe
 
 
@@ -17,55 +12,45 @@ class Validator[T](AutoPipe[[T], None]):
         assert is_data_model(cls), 'only support data model validation'
         
         self.cls = cls
-        self._serializer = Serializer(cls, {})
-        self._schema = AccessorSchema(cls)
+        self._schema = schema(cls)
     
     def run(self, object:T):
-        accessors = self._serializer.to_accessors(object)
+        realization = self._schema.realize(object)
+        error_fields = []
         notes = []
-        fields = []
-        for a, v in accessors.items():
-            schema_key = a
-            if re.search(r'\.\d+', a) is not None:
-                schema_key = re.sub(r'\.\d+', '.@idx', a)
-           
-            if schema_key not in self._schema:
-                notes.append(f'field {a} with value {v} does not conform to schema of {self.cls}')
-                fields.append(a)
-                continue
-
-            schema_field = self._schema[schema_key]
-            if isinstance(schema_field, List):
-                invalid_notes = [*_validate_list(a, schema_field.cls, v)]
-                if len(invalid_notes) > 0:
-                    fields.append(a)
-                    notes.extend(invalid_notes)
+        for e in realization.endpoints:
+            value = realization.value(e)
+            rep = self._schema.real_to_rep(e)
+            origin, args = self._schema.rep.type(rep)
+            
+            field_name = 'f.'.join(str(a) for a in e)
+            if isinstance(origin, (tuple, list)):
+                enotes = [*_validate_list(field_name, args, value)]
+                error_fields.append(field_name)
+                notes.extend(enotes)
             else:
-                invalid_note = _validate_object(a, schema_field.cls, v)
-                if invalid_note is not None:
-                    notes.append(invalid_note)
-                    fields.append(a)
+                enotes = _validate_object(field_name, origin, value)
+                if enotes is not None:
+                    error_fields.append(field_name)
+                    notes.append(enotes)
         
-        if len(fields) > 0:
-            error = AttributeError(fields)
+        if len(error_fields) > 0:
+            error = AttributeError(*error_fields)
             for n in notes:
                 error.add_note(n)
-
             raise error
 
 
 def _validate_object(field_name, field_type:type, obj):
-    if isinstance(field_type, type) and issubclass(field_type, Enum) and not isinstance(obj, Enum):
-        return f'{obj} is not of enum {field_type} at field {field_name}'
-    elif obj is not None and not isinstance(obj, field_type):
+    if obj is not None and not isinstance(obj, field_type):
         return f'{obj} is not of instance {field_type} at field {field_name}'
 
 
-def _validate_list(field_name, field_type, field_value):    
+def _validate_list(field_name, args, field_value):    
     if not isinstance(field_value, (tuple, list)):
         yield f'{field_value} is not a list or tuple at {field_name}'
-    elif field_type is not None:
+    elif len(args) > 0:
         for i, v in enumerate(field_value):
-            invalid_note = _validate_object(f'{field_name} item {i}', field_type, v)
+            invalid_note = _validate_object(f'{field_name} item {i}', args[0], v)
             if invalid_note is not None:
                 yield invalid_note
