678e395fe3492619f3018e89a2a66d890edea9b9
jokingbear <nerokid1072@gmail.com>
2026-03-24 03:09:12 +0000

    add stream

--- plasma/data_model/collections/__init__.py
+++ plasma/data_model/collections/__init__.py
@@ -1,3 +1,4 @@
 from .sorted_sequence import sorted
 from .dual_map import DualMap
 from .group import groupby
+from .stream import Stream
---
+++ plasma/data_model/collections/stream.py
@@ -0,0 +1,49 @@
+from itertools import tee
+from typing import Callable, Iterable
+
+from .group import groupby
+
+
+class Stream[T]:
+    
+    def __init__(self, data:Iterable[T]):
+        self._data = data
+    
+    def select[V](self, selector:Callable[[T], V]):
+        return Stream(selector(d) for d in self)
+
+    def filter(self, *filters:Callable[[T], bool]):
+        return Stream(d for d in self if all(f(d) for f in filters))
+    
+    def accumulate[S, D](self, initial_state:S, 
+                         selector:Callable[[T], D],
+                         accumulator:Callable[[D], S], stateful=True):
+        state = initial_state
+        for d in self:
+            temp_data = selector(d)
+            new_state = accumulator(temp_data)
+            if not stateful:
+                state = new_state
+        
+        return state
+    
+    def unwind[V](self, func:Callable[[T], Iterable[V]]):
+        return Stream(v for d in self 
+                      for v in func(d))
+    
+    def groupby[K, V](self, key:Callable[[T], K], value:Callable[[T], V]=None, as_stream=True):
+        group = groupby(self, key, value)
+        
+        if as_stream:
+            return Stream(group.items())
+        else:
+            return group
+    
+    def _clone(self):
+        iter1, iter2 = tee(self._data)
+        self._data = iter1
+        return iter2
+
+    def __iter__(self):
+        for d in self._clone():
+            yield d
