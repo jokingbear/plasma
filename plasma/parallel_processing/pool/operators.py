109fa3845e732bd4e164d6de6109014fcec2b5fa
jokingbear <nerokid1072@gmail.com>
2026-03-31 06:30:22 +0000

    add unwind distributor

--- plasma/parallel_processing/pool/operators.py
+++ plasma/parallel_processing/pool/operators.py
@@ -1,7 +1,7 @@
 from typing import Callable, Iterable
 from .utils import Invalid, End
 from ..communicators.accumulators import DynamicAccumulator
-from ..communicators.distributors import IteratorDistributor
+from ..communicators.distributors import Distributor
 from ...functional.decorators import propagate
 
 
@@ -24,7 +24,22 @@ class Unwinder[I, O](Operator[I, O]):
     
     def __init__(self, func:Callable[[I], Iterable[O]]):
         super().__init__()
-        self.pipe = IteratorDistributor()
+        self.pipe = self.UnwindDistributor(func)
+
+    class UnwindDistributor(Distributor):
+        
+        def __init__(self, func:Callable[..., Iterable]):
+            super().__init__()
+            self.func = func
+        
+        def run(self, data, *queues, **named_queues):
+            if data is End or data is Invalid:
+                for q in queues:
+                    q.put(data)
+            else:
+                for q in queues:
+                    for d in self.func(data):
+                        q.put(d)
 
 
 class Groupby[I, K, V](Operator[I, tuple[K, tuple[V]]]):
