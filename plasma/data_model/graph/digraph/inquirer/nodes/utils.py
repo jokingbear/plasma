b0d3de462ee5777b2d5cc31db27ccc6315ffdd93
jokingbear <nerokid1072@gmail.com>
2026-04-15 17:25:03 +0000

    refactor by pylance

---
+++ plasma/data_model/graph/digraph/inquirer/nodes/utils.py
@@ -0,0 +1,35 @@
+import inspect
+
+from typing import Callable, Hashable
+from .tuple_dict import TupleDict
+from .....base_model import Field
+
+
+def compile[T](
+        inquirer_type:type[T], 
+        selector:str|Field|Callable[..., object]
+    ) -> str|Field|Callable[[Hashable, T, TupleDict], object]:
+    if not callable(selector):
+        return selector
+    
+    signature = inspect.signature(selector)
+    has_args = any(p.kind is inspect._ParameterKind.VAR_POSITIONAL 
+                for p in signature.parameters.values())
+    parameters = {k:p for k, p in signature.parameters.items() if k != 'self'}
+    
+    if has_args or len(parameters) > 3:
+        return selector
+    elif len(parameters) == 2:
+        def alt_func(node:Hashable, inquirer:T, td:TupleDict):
+            return selector(node, inquirer)
+        
+        return alt_func
+    else:
+        def alt_func(node:Hashable, inquirer:T, td:TupleDict):
+            return selector(node)
+
+        return alt_func
+
+
+def select_node(node, _):
+    return node
