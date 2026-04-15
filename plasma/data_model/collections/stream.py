e98888245368cc22339c23a7c08bd130f332b521
jokingbear <nerokid1072@gmail.com>
2026-04-15 17:54:30 +0000

    refactor code

--- plasma/data_model/collections/stream.py
+++ plasma/data_model/collections/stream.py
@@ -15,9 +15,11 @@ class Stream[T]:
     def filter(self, *filters:Callable[[T], bool]):
         return Stream(d for d in self if all(f(d) for f in filters))
     
-    def accumulate[S, D](self, initial_state:S, 
-                         selector:Callable[[T], D],
-                         accumulator:Callable[[S, D], S], stateful=True):
+    def accumulate[S, D](
+            self, initial_state:S, 
+            selector:Callable[[T], D],
+            accumulator:Callable[[S, D], S], stateful=True
+        ):
         state = initial_state
         for d in self:
             temp_data = selector(d)
@@ -31,12 +33,12 @@ class Stream[T]:
         return Stream(v for d in self 
                       for v in func(d))
     
-    def groupby[K, V](self, key:Callable[[T], K], value:Callable[[T], V]=None):
+    def groupby[K, V](self, key:Callable[[T], K], value:Callable[[T], V]=None): # type:ignore
         group = groupby(self, key, value)
         return Stream(group.items())
     
-    def sort[K](self, key:Callable[[T], K]=None, reverse=False):
-        return Stream[T](sorted(self, key=key, reverse=reverse))
+    def sort[K](self, key:Callable[[T], K], reverse=False):
+        return Stream[T](sorted(self, key=key, reverse=reverse)) # type:ignore
     
     def evaluate(self):
         return [*self]
@@ -49,11 +51,11 @@ class Stream[T]:
         return True
     
     @staticmethod
-    def from_iterable[T](iterable:Iterable[Iterable[T]]):
+    def from_iterable[K](iterable:Iterable[Iterable[K]]):
         return Stream(chain.from_iterable(iterable))
     
     @staticmethod
-    def chain[T](*data:Iterable[T]):
+    def chain[K](*data:Iterable[K]):
         return Stream(chain(*data))
     
     def _clone(self):
