12c6379ea67c84190171eb32253418fe3a0fa7fb
jokingbear <nerokid1072@gmail.com>
2026-04-30 15:06:40 +0000

    update version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(requirement_path) as f:
 
 setup(
     name='plasma',
-    version='4.15.48dev4',
+    version='4.15.48dev5',
     packages=[*packages],
     license='MIT',
     author='jokingbear',
