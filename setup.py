ef25c466694359094c6ef47432991be26a2ed7fe
jokingbear <nerokid1072@gmail.com>
2026-05-13 05:03:57 +0000

    fix incorrect

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(requirement_path) as f:
 
 setup(
     name='plasma',
-    version='4.20.69a8',
+    version='4.20.69a9',
     packages=[*packages],
     license='MIT',
     author='jokingbear',
