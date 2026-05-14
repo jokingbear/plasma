fb5f038753d2306cbf612337c28568959a3b5f8a
jokingbear <nerokid1072@gmail.com>
2026-05-14 04:00:27 +0000

    expose frequently use classes

--- plasma/parallel_processing/__init__.py
+++ plasma/parallel_processing/__init__.py
@@ -2,3 +2,10 @@ import threading
 import multiprocessing as mp
 
 from . import communicators, queues
+from .communicators import (
+    AsyncFlow, Accumulator, accumulators, distributors
+)
+from .queues import (
+    Queue, ThreadQueue, ProcessQueue,
+    TransferQueue
+)
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(requirement_path) as f:
 
 setup(
     name='plasma',
-    version='4.20.69a9',
+    version='4.20.70a9',
     packages=[*packages],
     license='MIT',
     author='jokingbear',
