0fff9403d52c6fab8932bd5702bc2288e5eaef00
jokingbear <nerokid1072@gmail.com>
2026-05-16 15:42:23 +0000

    refactor provider

--- plasma/data_model/base_model/parser/provider.py
+++ plasma/data_model/base_model/parser/provider.py
@@ -1,19 +1,23 @@
 from typing import Callable, Any
+from warnings import deprecated
 
 from .base import Parser
-from ....functional import AutoPipe
 
 
-class ParsingProvider(AutoPipe):
+class ParsingProvider:
     
     def __init__(self):
         super().__init__()
         
-        self.type_parser = dict[type, Callable[[object], object]]()
+        self._sub_parsers = dict[type, Callable[[object], object]]()
     
+    def to[T](self, cls:type[T]):
+        return Parser[T](cls, self._sub_parsers)
+
+    @deprecated('use provider.to instead')
     def run[T](self, cls:type[T]):
-        return Parser[T](cls, self.type_parser)
+        return Parser[T](cls, self._sub_parsers)
 
     def register[T](self, cls:type[T], parser:Callable[[Any], T|None]):
-        self.type_parser[cls] = parser
+        self._sub_parsers[cls] = parser
         return self
