5118b7239fbb8b8fc168d1523872ea0cd39f1d6c
jokingbear <nerokid1072@gmail.com>
2026-05-17 05:01:10 +0000

    return self on  update

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(requirement_path) as f:
 
 setup(
     name='plasma',
-    version='4.22.75a19',
+    version='4.22.75a20',
     packages=[*packages],
     license='MIT',
     author='jokingbear',
