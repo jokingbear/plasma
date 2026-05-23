51dae692bf5d0d3a4acce1007e7092af430f9d6a
jokingbear <nerokid1072@gmail.com>
2026-05-23 16:39:54 +0000

    refactor code

--- plasma/parallel_processing/communicators/distributors/base.py
+++ plasma/parallel_processing/communicators/distributors/base.py
@@ -4,6 +4,6 @@ from ...queues import Queue
 class Distributor[T]:
 
     def run(self, data:T, *queues:Queue, **named_queues:Queue):...
-
+    
     def __repr__(self):
         return f'{type(self)}'
