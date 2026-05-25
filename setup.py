3422181e6e1faa7fae176e21eba114e5339d3366
simon.huynh <nerokid1072@gmail.com>
2026-05-25 23:25:21 +0700

    raise child process error

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(requirement_path) as f:
 
 setup(
     name='plasma',
-    version='4.27.91a5',
+    version='4.27.91dev6',
     packages=[*packages],
     license='MIT',
     author='jokingbear',
