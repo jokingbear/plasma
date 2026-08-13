7d6cd6f251b3668434433075a29ff8d8a81b22ae
jokingbear <jokingbear>
2026-08-13 04:39:34 +0000

    change lru cache usage

--- plasma/storages/sqlite3.py
+++ plasma/storages/sqlite3.py
@@ -2,20 +2,44 @@ import threading
 
 from sqlite3 import Connection
 from functools import lru_cache
+from ..functional import partial_right
 
 
 class SqliteStorage:
     
     def __init__(self, filepath:str, max_connection:int=32):
         self.filepath = filepath
-        self._init_connection = lru_cache(maxsize=max_connection)(
-            self._init_connection, 
-        )
+        
+        connection_initator = lru_cache(maxsize=max_connection)(_init_connection)
+        connection_initator = partial_right(connection_initator, filepath)
+        self._init_connection = connection_initator
 
     @property
     def connection(self):
         thread_id = threading.get_ident()
         return self._init_connection(thread_id)
     
-    def _init_connection(self, thread_id):
-        return Connection(self.filepath)
+    def query(self, statement:str):
+        return Query(self, statement)
+
+
+def _init_connection(filepath:str, thread_id:int):
+    return Connection(filepath)
+
+
+class Query:
+    
+    def __init__(self, storage:SqliteStorage, query:str):
+        self.storage = storage
+        self.query = query
+    
+    def executemany(self, params):
+        with self.storage.connection as conn:
+            conn.executemany(self.query, params)
+    
+    def execute(self, params):
+        with self.storage.connection as conn:
+            return conn.execute(self.query, params)
+    
+    def __repr__(self):
+        return self.query
