90328fe2c95c1f255ed574b540b3248e3b301b48
jokingbear <jokingbear>
2026-09-14 13:41:44 +0000

    test increase version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open(requirement_path) as f:
 
 setup(
     name='plasma',
-    version='4.34.9dev2',
+    version='4.34.9dev3',
     packages=[*packages],
     license='MIT',
     author='jokingbear',
